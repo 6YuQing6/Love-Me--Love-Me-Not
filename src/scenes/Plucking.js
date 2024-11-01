f8e31b955fbf2b5cb537121f8c8a037330171f9d
Graham Taylor <gktaylor@ucsc.edu>
2024-11-01 03:52:05 -0700

    dont check the commit time

--- src/scenes/Plucking.js
+++ src/scenes/Plucking.js
@@ -6,19 +6,59 @@ class Pluck extends Phaser.Scene {
   create() {
     cursors = this.input.keyboard.createCursorKeys();
     let graphics = this.add.graphics();
-    graphics.fillStyle(0xffffff, 1);
+    graphics.fillStyle(0x000000, 0.5);
     graphics.fillRect(
-      this.cameras.main.width / 4,
-      this.cameras.main.height / 4,
-      this.cameras.main.width / 2,
-      this.cameras.main.height / 2
+      0,
+      0,
+      this.cameras.main.width,
+      this.cameras.main.height
     );
+    this.thewholeflower = []
+    this.thewholeflowerpositions = [[70,-110],[-70,-110],[100,0],[-100,0],[0,85],[0,0]]
+    for(let i = 0; i < 6; i += 1){
+      this.thewholeflower.push(this.physics.add.sprite(this.scale.width/2 + this.thewholeflowerpositions[i][0],this.scale.height/2 + this.thewholeflowerpositions[i][1],'petal-blue', i))
+      if(i < 5){
+        this.makeItDrag(this.thewholeflower[i])
+      }
+    }
   }
 
   update() {
+    let count = 0
+    for(let i = 0; i < 5; i += 1){
+      if(this.thewholeflower[i].y > this.scale.height){
+        count += 1
+      }
+    }
+    if(count == 5){
+      this.scene.resume("fieldScene");
+      this.scene.stop();
+    }
     if (Phaser.Input.Keyboard.JustDown(cursors.space)) {
       this.scene.resume("fieldScene");
       this.scene.stop();
     }
+  }
+  // code from https://www.youtube.com/watch?app=desktop&v=jWglIBp4usY
+  makeItDrag(gameObject){
+    gameObject.setInteractive()
+
+    function startDrag(){
+      gameObject.off(Phaser.Input.Events.POINTER_DOWN, startDrag)
+      gameObject.on(Phaser.Input.Events.POINTER_UP, stopDrag)
+      gameObject.on(Phaser.Input.Events.POINTER_MOVE, onDrag)
+    }
+    function stopDrag(){
+      gameObject.off(Phaser.Input.Events.POINTER_DOWN, startDrag)
+      gameObject.off(Phaser.Input.Events.POINTER_UP, stopDrag)
+      gameObject.off(Phaser.Input.Events.POINTER_MOVE, onDrag)
+      gameObject.setGravityY(1000)
+    }
+    function onDrag(pointer){
+      gameObject.x = pointer.x;
+      gameObject.y = pointer.y;
+    }
+
+    gameObject.on(Phaser.Input.Events.POINTER_DOWN, startDrag)
   }
 }
